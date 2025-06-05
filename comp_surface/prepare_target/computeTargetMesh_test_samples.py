3cea2bc43cd66271fde88d730a533b46d50dd869
benk568 <ben.kaminow@schrodinger.com>
2025-06-05 16:28:08 -0400

    More cleanup.

--- comp_surface/prepare_target/computeTargetMesh_test_samples.py
+++ comp_surface/prepare_target/computeTargetMesh_test_samples.py
@@ -6,7 +6,7 @@ import shutil
 import glob
 import pymesh
 import Bio.PDB
-from Bio.PDB import * 
+from Bio.PDB import *
 from rdkit import Chem
 import warnings
 warnings.filterwarnings("ignore")
@@ -45,45 +45,39 @@ def asl_to_pocket_selection(asl: str) -> str:
 
 
 def compute_inp_surface(
-    target_filename, ligand_filename, out_dir=None, dist_threshold=10, pocket_asl=""
+    target_filename, ligand_filename, out_dir, dist_threshold=10, pocket_asl=""
 ):
     try:
+        out_dir = out_dir / target_filename.parent.name
+        out_dir.mkdir(exist_ok=True)
 
-        sufix = '_'+str(dist_threshold)+'A.pdb'
-        # out_filename = os.path.splitext(target_filename)[0]
-        if out_dir is not None: 
-            out_filename = os.path.join(out_dir,ligand_filename.split('/')[-2]) 
-            os.makedirs(out_filename,exist_ok=True)
-            sufix = '/' + os.path.splitext(target_filename)[0].split('/')[-1] + '_'+str(dist_threshold)+'A.pdb'
-        else:
-            out_filename = os.path.splitext(ligand_filename)[0]
-        if os.path.exists(out_filename+f"/{sufix.split('.')[0]}.ply"):
-            print('have done skip!')
+        # sufix = '/' + os.path.splitext(target_filename)[0].split('/')[-1] + '_'+str(dist_threshold)+'A.pdb'
+
+        # This is the file we ultimately want, can just skip everything if it already
+        #  exists
+        ply_out_path = out_dir / f"{target_filename.name.split('.')[0]}_{dist_threshold}A.ply"
+        if ply_out_path:
+            print(f"{ply_out_path} already computed", flush=True)
             return 0
-        input_filename = os.path.splitext(target_filename)[0]
-        # Get atom coordinates
-        # try:
-        #     mol = Chem.MolFromMol2File(ligand_filename, sanitize=False, cleanupSubstructures=False)
-        # except:
-        #     print('mol2 faild try sdf')
-        if ligand_filename.endswith('.sdf'):
+
+        if ligand_filename.suffix == ".sdf":
             mol = Chem.SDMolSupplier(ligand_filename, sanitize=False)[0]
-        elif ligand_filename.endswith('.pdb'):
+        elif ligand_filename.suffix == ".pdb":
             mol = Chem.MolFromPDBFile(ligand_filename, sanitize=False)
         g = mol_to_nx(mol)
         atomCoords = np.array([g.nodes[i]['pos'].tolist() for i in g.nodes])
-        
+
         # Read protein and select aminino acids in the binding pocket
         parser = Bio.PDB.PDBParser(QUIET=True) # QUIET=True avoids comments on errors in the pdb.
 
-        structures = parser.get_structure('target', input_filename+'.pdb')
+        structures = parser.get_structure('target', target_filename)
         structure = structures[0] # 'structures' may contain several proteins in this case only one.
 
         atoms  = Bio.PDB.Selection.unfold_entities(structure, 'A')
         ns = Bio.PDB.NeighborSearch(atoms)
-        
+
         close_residues= []
-        for a in atomCoords:  
+        for a in atomCoords:
             close_residues.extend(ns.search(a, dist_threshold, level='R'))
         close_residues = Bio.PDB.Selection.uniqueify(close_residues)
 
@@ -100,7 +94,7 @@ def compute_inp_surface(
         pdbio = PDBIO()
         pdbio.set_structure(structure)
         pdbio.save(out_filename+sufix, SelectNeighbors())
-        
+
         # Identify closes atom to the ligand
         structures = parser.get_structure('target', out_filename+sufix)
         structure = structures[0] # 'structures' may contain several proteins in this case only one.
@@ -110,39 +104,39 @@ def compute_inp_surface(
         #atom_idx = np.argmin(dist)
         #dist = [[distance.euclidean(ac, a.get_coord()) for ac in atomCoords] for a in atoms]
         #atom_idx = np.argsort(np.min(dist, axis=1))[0]
-        
-        # Compute MSMS of surface w/hydrogens, 
+
+        # Compute MSMS of surface w/hydrogens,
         try:
             dist = [distance.euclidean(atomCoords.mean(axis=0), a.get_coord()) for a in atoms]
             atom_idx = np.argmin(dist)
             vertices1, faces1, normals1, names1, areas1 = computeMSMS(out_filename+sufix,\
                                                                     protonate=True, one_cavity=atom_idx)
-            
+
             # Find the distance between every vertex in binding site surface and each atom in the ligand.
             kdt = KDTree(atomCoords)
             d, r = kdt.query(vertices1)
             assert(len(d) == len(vertices1))
             iface_v = np.where(d <= dist_threshold-5)[0]
-            faces_to_keep = [idx for idx, face in enumerate(faces1) if all(v in iface_v  for v in face)] 
-        
+            faces_to_keep = [idx for idx, face in enumerate(faces1) if all(v in iface_v  for v in face)]
+
             # Compute "charged" vertices
             if masif_opts['use_hbond']:
-                vertex_hbond = computeCharges(input_filename, vertices1, names1)    
-        
-            # For each surface residue, assign the hydrophobicity of its amino acid. 
+                vertex_hbond = computeCharges(input_filename, vertices1, names1)
+
+            # For each surface residue, assign the hydrophobicity of its amino acid.
             if masif_opts['use_hphob']:
-                vertex_hphobicity = computeHydrophobicity(names1)    
-            
+                vertex_hphobicity = computeHydrophobicity(names1)
+
             # If protonate = false, recompute MSMS of surface, but without hydrogens (set radius of hydrogens to 0).
             vertices2 = vertices1
             faces2 = faces1
-        
+
             # Fix the mesh.
             mesh = pymesh.form_mesh(vertices2, faces2)
             mesh = pymesh.submesh(mesh, faces_to_keep, 0)
             with io.capture_output() as captured:
                 regular_mesh = fix_mesh(mesh, masif_opts['mesh_res'])
-            
+
         except:
             try:
                 dist = [[distance.euclidean(ac, a.get_coord()) for ac in atomCoords] for a in atoms]
@@ -155,26 +149,26 @@ def compute_inp_surface(
                 d, r = kdt.query(vertices1)
                 assert(len(d) == len(vertices1))
                 iface_v = np.where(d <= dist_threshold-5)[0]
-                faces_to_keep = [idx for idx, face in enumerate(faces1) if all(v in iface_v  for v in face)] 
-        
+                faces_to_keep = [idx for idx, face in enumerate(faces1) if all(v in iface_v  for v in face)]
+
                 # Compute "charged" vertices
                 if masif_opts['use_hbond']:
-                    vertex_hbond = computeCharges(input_filename, vertices1, names1)    
-        
-                # For each surface residue, assign the hydrophobicity of its amino acid. 
+                    vertex_hbond = computeCharges(input_filename, vertices1, names1)
+
+                # For each surface residue, assign the hydrophobicity of its amino acid.
                 if masif_opts['use_hphob']:
-                    vertex_hphobicity = computeHydrophobicity(names1)    
-            
+                    vertex_hphobicity = computeHydrophobicity(names1)
+
                 # If protonate = false, recompute MSMS of surface, but without hydrogens (set radius of hydrogens to 0).
                 vertices2 = vertices1
                 faces2 = faces1
-        
+
                 # Fix the mesh.
                 mesh = pymesh.form_mesh(vertices2, faces2)
                 mesh = pymesh.submesh(mesh, faces_to_keep, 0)
                 with io.capture_output() as captured:
                     regular_mesh = fix_mesh(mesh, masif_opts['mesh_res'])
-                    
+
             except:
                 vertices1, faces1, normals1, names1, areas1 = computeMSMS(out_filename+sufix,\
                                                                         protonate=True, one_cavity=None)
@@ -184,43 +178,43 @@ def compute_inp_surface(
                 d, r = kdt.query(vertices1)
                 assert(len(d) == len(vertices1))
                 iface_v = np.where(d <= dist_threshold-5)[0]
-                faces_to_keep = [idx for idx, face in enumerate(faces1) if all(v in iface_v  for v in face)] 
-        
+                faces_to_keep = [idx for idx, face in enumerate(faces1) if all(v in iface_v  for v in face)]
+
                 # Compute "charged" vertices
                 if masif_opts['use_hbond']:
-                    vertex_hbond = computeCharges(input_filename, vertices1, names1)    
-        
-                # For each surface residue, assign the hydrophobicity of its amino acid. 
+                    vertex_hbond = computeCharges(input_filename, vertices1, names1)
+
+                # For each surface residue, assign the hydrophobicity of its amino acid.
                 if masif_opts['use_hphob']:
-                    vertex_hphobicity = computeHydrophobicity(names1)    
-            
+                    vertex_hphobicity = computeHydrophobicity(names1)
+
                 # If protonate = false, recompute MSMS of surface, but without hydrogens (set radius of hydrogens to 0).
                 vertices2 = vertices1
                 faces2 = faces1
-        
+
                 # Fix the mesh.
                 mesh = pymesh.form_mesh(vertices2, faces2)
                 mesh = pymesh.submesh(mesh, faces_to_keep, 0)
                 with io.capture_output() as captured:
                     regular_mesh = fix_mesh(mesh, masif_opts['mesh_res'])
-            
+
         # Compute the normals
         vertex_normal = compute_normal(regular_mesh.vertices, regular_mesh.faces)
         # Assign charges on new vertices based on charges of old vertices (nearest
         # neighbor)
-        
+
         if masif_opts['use_hbond']:
             vertex_hbond = assignChargesToNewMesh(regular_mesh.vertices, vertices1,\
                 vertex_hbond, masif_opts)
-        
+
         if masif_opts['use_hphob']:
             vertex_hphobicity = assignChargesToNewMesh(regular_mesh.vertices, vertices1,\
                 vertex_hphobicity, masif_opts)
-        
+
         if masif_opts['use_apbs']:
             vertex_charges = computeAPBS(regular_mesh.vertices, out_filename+sufix, out_filename+"_temp")
-            
-        # Compute the principal curvature components for the shape index. 
+
+        # Compute the principal curvature components for the shape index.
         regular_mesh.add_attribute("vertex_mean_curvature")
         H = regular_mesh.get_attribute("vertex_mean_curvature")
         regular_mesh.add_attribute("vertex_gaussian_curvature")
@@ -231,21 +225,21 @@ def compute_inp_surface(
         elem[elem<0] = 1e-8
         k1 = H + np.sqrt(elem)
         k2 = H - np.sqrt(elem)
-        # Compute the shape index 
+        # Compute the shape index
         si = (k1+k2)/(k1-k2)
         si = np.arctan(si)*(2/np.pi)
-        
+
         # Convert to ply and save.
         save_ply(out_filename+f"/{sufix.split('.')[0]}.ply", regular_mesh.vertices,\
                 regular_mesh.faces, normals=vertex_normal, charges=vertex_charges,\
                 normalize_charges=True, hbond=vertex_hbond, hphob=vertex_hphobicity,\
                 si=si)
-        
+
         return 0
     except:
         return target_filename
 
-    
+
 if __name__ == "__main__":
     from joblib import delayed,Parallel
     # arguments
@@ -280,8 +274,7 @@ if __name__ == "__main__":
             lig_path = protein_dir / f"{target_name}_ligand.mol2"
         if not lig_path.exists():
             print(
-                f"No ligand found for {target_name}, using passed pocket_asl.",
-                flush=True,
+                f"No ligand found for {target_name}, using pocket_asl.", flush=True
             )
             lig_path = None
         args_list.append((prot_path, lig_path, args.pocket_asl))
@@ -298,7 +291,7 @@ if __name__ == "__main__":
         )
         for (prot_path, lig_path, pocket_asl) in tqdm(args_list)
     )
-    
+
     # Delete all temp files generated in the process
     for f in args.out_dir.glob("*_temp*"):
         f.unlink()
